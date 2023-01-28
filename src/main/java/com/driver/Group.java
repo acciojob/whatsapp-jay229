7fab32c75296233462e972a0d86264cbdbb0c2be
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2023-01-28 18:00:30 +0530

    Solution getters and setters added

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -8,4 +8,20 @@ public class Group {
         this.name = name;
         this.numberOfParticipants = numberOfParticipants;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
